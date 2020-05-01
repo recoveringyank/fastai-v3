8813b7c322283602e396afb5e2b5e46a7d630699
austin <rabbenn@cnic.es>
2020-05-01 23:40:58 +0200

    T1T12_classifier

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1-3-b1PR7Svj2U4i7J5lGg4gdD2PcQgLY'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['T1', 'T2']
 path = Path(__file__).parent
 
 app = Starlette()
